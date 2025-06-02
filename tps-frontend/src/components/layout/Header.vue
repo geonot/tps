2ce908ef617b1b42f9a4ca51d43285d74196c66f
Rome Stone <fractalattice@gmail.com>
2025-06-01 21:26:30 -0700

    updates

--- tps-frontend/src/components/layout/Header.vue
+++ tps-frontend/src/components/layout/Header.vue
@@ -1,8 +1,12 @@
 <template>
   <header class="bg-primaryBlue text-white p-4 shadow-md">
     <div class="container mx-auto flex justify-between items-center">
-      <div class="text-xl font-headings font-bold">
-        TPS Logo
+      <div class="flex items-center space-x-3">
+        <img 
+          :src="logoUrl" 
+          alt="TPS Logo" 
+          class="h-10 w-auto"
+        />
       </div>
       <Navigation />
     </div>
@@ -11,12 +15,18 @@
 
 <script>
 import Navigation from './Navigation.vue';
+import logoImage from '@/assets/logo.jpeg';
 
 export default {
   name: 'AppHeader',
   components: {
     Navigation,
   },
+  data() {
+    return {
+      logoUrl: logoImage
+    };
+  }
 };
 </script>
 
